bad04bd1bd63b1baf9d6df66cc655103c3cb91ef
chianghsinhao <chianghsinhao@gmail.com>
2018-11-11 00:54:57 -0800

    Use BookItem in BookShelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,42 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import BookItem from './BookItem'
 
 class BookShelf extends Component {
   static propTypes = {
-    shelfName: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     books: PropTypes.array.isRequired,
-    moveShelf: PropTypes.func.isRequired
+    moveToShelf: PropTypes.func.isRequired
   }
 
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+        <h2 className="bookshelf-title">{this.props.name}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.props.books.map((book) => (
-                <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div
-                        className="book-cover"
-                        style={{ width: 128, height: 193, backgroundImage: "url(" + book.imageLinks.thumbnail + ")" }}>
-                      </div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors.join(", ")}</div>
-                  </div>
-                </li>
-            ))}
+            {this.props.books.length? this.props.books.map((book) => (
+                <BookItem
+                  key={book.id}
+                  book={book}
+                  moveToShelf={this.props.moveToShelf}
+                />
+            )): (<span>Nothing in shelf!</span>)}
           </ol>
         </div>
       </div>
