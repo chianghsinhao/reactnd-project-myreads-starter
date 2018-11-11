71af0af65fb56a4a721db73f82602af23ff9a44c
chianghsinhao <chianghsinhao@gmail.com>
2018-11-11 04:17:24 -0800

    Handle exceptions for BookItem

--- src/BookItem.js
+++ src/BookItem.js
@@ -15,11 +15,17 @@ class BookItem extends Component {
           <div className="book-top">
             <div
               className="book-cover"
-              style={{ width: 128, height: 193, backgroundImage: "url(" + book.imageLinks.thumbnail + ")" }}>
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: (book.imageLinks && book.imageLinks.thumbnail)?
+                                 "url(" + book.imageLinks.thumbnail + ")" :
+                                 ""
+              }}>
             </div>
             <div className="book-shelf-changer">
-              <select value={book.shelf} onChange={(e) => {
-                  this.props.moveToShelf(book.id, e.target.value)
+              <select value={book.shelf? book.shelf: 'none'} onChange={(e) => {
+                  this.props.moveToShelf(book, e.target.value)
               }}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
@@ -30,7 +36,7 @@ class BookItem extends Component {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors.join(", ")}</div>
+          <div className="book-authors">{book.authors? book.authors.join(", "): ''}</div>
         </div>
       </li>
     )
